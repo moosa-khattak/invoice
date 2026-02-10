9f8001786764173d806444d355338625634e7bfc
MUhammad Moosa <moosakhattak333@gmail.com>
2026-02-09 22:39:03 -0800

    feat: introduce CurrencySeeder to populate common currency records.

--- database/seeders/CurrencySeeder.php
+++ database/seeders/CurrencySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Currency;
 
 class CurrencySeeder extends Seeder
 {
@@ -12,8 +13,18 @@ class CurrencySeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Currency::create(['code' => 'USD', 'symbol' => '$', 'name' => 'US Dollar']);
-        \App\Models\Currency::create(['code' => 'EUR', 'symbol' => '€', 'name' => 'Euro']);
-        \App\Models\Currency::create(['code' => 'UGX', 'symbol' => 'USh', 'name' => 'Ugandan Shilling']);
+       Currency::create(['code' => 'USD', 'symbol' => '$', 'name' => 'US Dollar']);
+Currency::create(['code' => 'EUR', 'symbol' => '€', 'name' => 'Euro']);
+Currency::create(['code' => 'GBP', 'symbol' => '£', 'name' => 'British Pound']);
+Currency::create(['code' => 'JPY', 'symbol' => '¥', 'name' => 'Japanese Yen']);
+Currency::create(['code' => 'AUD', 'symbol' => 'A$', 'name' => 'Australian Dollar']);
+Currency::create(['code' => 'CAD', 'symbol' => 'C$', 'name' => 'Canadian Dollar']);
+Currency::create(['code' => 'CHF', 'symbol' => 'CHF', 'name' => 'Swiss Franc']);
+Currency::create(['code' => 'CNY', 'symbol' => '¥', 'name' => 'Chinese Yuan']);
+Currency::create(['code' => 'PKR', 'symbol' => '₨', 'name' => 'Pakistani Rupee']);
+Currency::create(['code' => 'INR', 'symbol' => '₹', 'name' => 'Indian Rupee']);
+Currency::create(['code' => 'NZD', 'symbol' => 'NZ$', 'name' => 'New Zealand Dollar']);
+Currency::create(['code' => 'SGD', 'symbol' => 'S$', 'name' => 'Singapore Dollar']);
+
     }
 }
